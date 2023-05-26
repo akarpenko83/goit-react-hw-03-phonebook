f6749b9e23a99e74d1e04887bd726b87ff9c172e
Anton K <akarpenko83@gmail.com>
2023-05-26 15:19:55 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,52 @@ import Filter from './phonebook/Filter';
 import { SectionName } from './phonebook/Phonebook.styled';
 import background from './utils/background';
 class App extends Component {
+<<<<<<< HEAD
     state = {};
+=======
+    state = {
+        contacts: INITIAL_CONTACTS,
+        filter: '',
+    };
+
+    onSubmit = contact => {
+        if (
+            this.state.contacts.find(
+                arr => arr.name === contact.name,
+            )
+        ) {
+            alert(
+                `${contact.name} is already in the contact list`,
+            );
+            return;
+        }
+        this.setState(prevState => ({
+            contacts: [...this.state.contacts, contact],
+        }));
+    };
+
+    handleRemoveContact = contactId => {
+        this.setState(prevState => ({
+            contacts: prevState.contacts.filter(
+                contact => contact.id !== contactId,
+            ),
+        }));
+    };
+
+    handleChangeFilter = evt => {
+        this.setState({ filter: evt.target.value });
+    };
+
+    getFilteredContacts = () => {
+        const normalizedFilter =
+            this.state.filter.toLowerCase();
+        return this.state.contacts.filter(contact =>
+            contact.name
+                .toLowerCase()
+                .includes(normalizedFilter),
+        );
+    };
+>>>>>>> parent of db27ad9 (localstorage done)
 
     render() {
         return 'return';
--- src/components/phonebook/Contact.jsx
+++ src/components/phonebook/Contact.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import PropTypes from 'prop-types';
 import { DeleteButton } from './Phonebook.styled';
 
@@ -20,3 +21,26 @@ Contact.propTypes = {
     number: PropTypes.string.isRequired,
     onDelete: PropTypes.func.isRequired,
 };
+=======
+import { nanoid } from 'nanoid';
+
+export default function Contact(
+    id = nanoid(),
+    name,
+    number,
+    onRemoveContact,
+) {
+    return (
+        <li key={id}>
+            {name}: {number}
+            {'   '}
+            <button
+                type="button"
+                onClick={() => onRemoveContact(id)}
+            >
+                Delete
+            </button>
+        </li>
+    );
+}
+>>>>>>> parent of db27ad9 (localstorage done)
